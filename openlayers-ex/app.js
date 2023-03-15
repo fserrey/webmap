cbdc5b2703aa8f5b021435f5e8fcbcb267381830
Fran <francisco.serrano@galeo.tech>
2023-03-15 18:06:09 +0100

    first commit - openlayers

---
+++ openlayers-ex/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index", { root: __dirname + '/views/' });
+  });
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
