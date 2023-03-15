f14507d3c10e183a39620f9b477dcf28d4b8090e
Fran <francisco.serrano@galeo.tech>
2023-03-15 10:13:21 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,159 @@
+
+//mapa
+var map = L.map('map', {
+  center: [40.4167754, -3.7037902],
+  zoom: 13
+});
+
+//capas
+var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap' +
+    '</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">' +
+    'CC-BY-SA</a>'
+}).addTo(map);
+
+var watercolor = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>,' +
+    ' under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.' +
+    ' Data by <a href="http://openstreetmap.org">OpenStreetMap</a>,' +
+    ' under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+});
+var terrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>,' +
+    ' under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.' +
+    ' Data by <a href="http://openstreetmap.org">OpenStreetMap</a>,' +
+    ' under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+});
+var stadia = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+  maxZoom: 19,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+});
+var toner = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+  maxZoom: 19,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+});
+var Spain_PNOA_Ortoimagen = L.tileLayer.wms('http://www.ign.es/wms-inspire/pnoa-ma', {
+  layers: 'OI.OrthoimageCoverage',
+  format: 'image/png',
+  transparent: false,
+  continuousWorld: true,
+  attribution: 'PNOA cedido por © <a href="http://www.ign.es/ign/main/index.do" target="_blank">Instituto Geográfico Nacional de España</a>'
+});
+var Spain_UnidadAdministrativa = L.tileLayer.wms('http://www.ign.es/wms-inspire/unidades-administrativas', {
+  layers: 'AU.AdministrativeUnit',
+  format: 'image/png',
+  transparent: true,
+  continuousWorld: true,
+  attribution: '© <a href="http://www.ign.es/ign/main/index.do" target="_blank">Instituto Geográfico Nacional de España</a>'
+
+});
+
+var lidar = L.tileLayer.providerESP('LiDAR');
+var curvas_de_nivel = L.tileLayer.providerESP('MDT.CurvasNivel');
+var nombres_geograficos = L.tileLayer.providerESP('NombresGeograficos');
+
+// Agregar plugin de edición
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+var drawControl = new L.Control.Draw({
+  edit: {
+    featureGroup: drawnItems
+  },
+  draw: {
+    polygon: true,
+    polyline: true,
+    rectangle: true,
+    circle: true,
+    marker: true
+  }
+});
+map.addControl(drawControl);
+
+// Cambiar estilo de los polígonos dibujados
+map.on('draw:created', function (e) {
+  var layer = e.layer;
+  layer.setStyle({
+    fillColor: '#00FF00',
+    fillOpacity: 0.5,
+    color: '#000000',
+    weight: 2
+  });
+  drawnItems.addLayer(layer);
+});
+
+// Cargar capa de puntos con omnivore y agregar pop-up con atributos
+// omnivore.csv('data/puntos.csv')
+//   .on('ready', function (layer) {
+//     this.eachLayer(function (marker) {
+//       marker.bindPopup('<strong>' + marker.toGeoJSON().properties.nombre + '</strong><br>' +
+//         marker.toGeoJSON().properties.descripcion);
+//     });
+//   })
+//   .addTo(map);
+
+// Agregar plugin de minimapa
+//var miniMap = new L.Control.MiniMap(osmLayer, {
+//  toggleDisplay: true,
+//  minimized: false
+//}).addTo(map);
+
+// Agregar plugin de localización
+// L.control.locate({
+//   strings: {
+//     title: "Mostrar mi ubicación"
+//   }
+// }).addTo(map);
+
+//controles
+L.control.scale({
+  position: 'bottomright',
+  imperial: false,
+
+}).addTo(map);
+
+var baseMaps = {
+  "Base de OpenStreetMap": osm,
+  "Acuarela": watercolor,
+  "Terreno": terrain,
+  "Modo oscuro": toner,
+  "Modo claro": stadia
+
+};
+
+var overlays = {
+  "Parques": parques,
+  "PNOA": Spain_PNOA_Ortoimagen,
+  "Unidades Administrativas": Spain_UnidadAdministrativa,
+  "LiDAR": lidar,
+  "Curvas de nivel": curvas_de_nivel,
+  "Nombres geográficos": nombres_geograficos,
+};
+
+var miniMap = new L.Control.MiniMap(L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'), {
+      toggleDisplay: true,
+      minimized: true,
+    })
+    
+//miniMap.addTo(map);
+
+
+L.control.layers(baseMaps, overlays).addTo(map);
+
+// var drawControl = new L.Control.Draw({
+//   edit: {
+//     featureGroup: capaEdicion
+//   }
+// });
+// 
+// 
+// map.addControl(drawControl);
+
+
+
